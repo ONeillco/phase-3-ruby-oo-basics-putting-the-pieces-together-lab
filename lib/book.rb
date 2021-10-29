bfcef2f4ec4229fcf6967f0da870279033d31532
Cory <oneillco117@gmail.com>
2021-10-29 17:01:08 -0400

    commit tree 37aaa2b64241ba5544cb8fe64558a65c18635baf

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+require 'pry'
 
+class Book
+attr_accessor :title, :author, :page_count, :genre  
 
+def initialize(title, author = "Agatha Christie", page_count = 272, genre = "Mystery")
+  self.title = title
+  @author = author
+  @page_count = page_count
+  @genre = genre
+  @current_page = 1
+ 
+  turn_page
+end
+
+# def author
+#   @author
+# end
+
+# def author=(author)
+#   @author = author
+# end 
+
+# def page_count=(page_count)
+#   @page_count
+# end
+
+# def genre=(genre)
+#   @genre
+# end
+
+def turn_page
+  
+  @current_page += 1
+ puts "Flipping the page...wow, you read fast!"
+end 
+end
